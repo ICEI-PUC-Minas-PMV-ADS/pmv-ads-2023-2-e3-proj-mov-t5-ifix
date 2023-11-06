33ceaca848c7853a6b899f8f3be7636df8a6c990
Felipe Carneiro <felipecarneiro@duck.com>
2023-11-06 00:30:03 -0300

    Update login2.js

--- src/screens/login2.js
+++ src/screens/login2.js
@@ -1,5 +1,5 @@
 
-import React ,{ useState } from "react"
+import React, { useState } from "react"
 import { Button, StyleSheet, Text, TextInput, View, Image } from "react-native";
 import { firebaseAuth } from "../external/infra/fireBaseConfig";
 import { ActivityIndicator } from "react-native";
@@ -17,7 +17,7 @@ const Login = () => {
     try {
       const response = await signInWithEmailAndPassword(auth, email, password);
       console.log(response)
-    } catch(err) {
+    } catch (err) {
       console.log(err)
     } finally {
       setLoading(false)
@@ -30,7 +30,7 @@ const Login = () => {
     try {
       const response = await createUserWithEmailAndPassword(auth, email, password);
       console.log(response)
-    } catch(err) {
+    } catch (err) {
       console.log(err)
     } finally {
       setLoading(false)
@@ -48,34 +48,40 @@ const Login = () => {
 
       <View style={styles.container}>
         <TextInput
-        style={styles.input}
-        value={email}
-        placeholder="Email"
-        autoCapitalize="none"
-        onChangeText={(text) => setEmail(text)}
-        left={<TextInput.Icon name="account" />}
+          style={styles.input}
+          value={email}
+          placeholder="Email"
+          autoCapitalize="none"
+          onChangeText={(text) => setEmail(text)}
+          left={<TextInput.Icon name="account" />}
+          keyboardType='email-address'
         />
 
         <TextInput
-        style={styles.input}
-        value={password}
-        placeholder="Password"
-        autoCapitalize="none"
-        secureTextEntry={true}
-        onChangeText={(text) => setPassword(text)}
-        left={<TextInput.Icon name="key" />}
+          style={styles.input}
+          value={password}
+          placeholder="Password"
+          autoCapitalize="none"
+          secureTextEntry={true}
+          onChangeText={(text) => setPassword(text)}
+          left={<TextInput.Icon name="key" />}
+          keyboardType='default'
         />
 
         {
-        loading  ? <ActivityIndicator size="large" color='#0000ff'/>
-        : (
-        <>
-          <Button title="Logins" onPress={signIn} style={styles.buttonLogin} mode="contained" />
-          <Button title="create account" onPress={signUp} style={styles.buttonCreate} mode="contained" />
-        </>
-        )}
+          loading ? <ActivityIndicator size="large" color='#0000ff' />
+            : (
+              <>
+                <Button title="Logins" onPress={signIn} style={styles.buttonLogin} mode="contained">
+                  Login
+                </Button>
+                <Button title="create account" onPress={signUp} style={styles.buttonCreate} mode="contained">
+                  Create Account
+                </Button>
+              </>
+            )}
       </View>
-    </View>
+    </View >
   )
 }
 
@@ -83,18 +89,17 @@ export default Login;
 
 const styles = StyleSheet.create({
   container: {
-    flex:1,
-    margin:10,
+    flex: 1,
+    margin: 10,
     justifyContent: 'center'
   },
 
   header: {
-//    alignItems: 'center',
     marginTop: 30,
     marginBottom: 12
   },
 
-   image:{
+  image: {
     width: 128,
     height: 128,
     borderRadius: 30
@@ -106,19 +111,20 @@ const styles = StyleSheet.create({
 
   input: {
     marginVertical: 4,
-    height:50,
-    borderWidth:1,
-    borderRadius:4,
-    padding:10,
+    height: 50,
+    borderWidth: 1,
+    borderRadius: 4,
+    padding: 10,
     backgroundColor: '#fff'
   },
 
   buttonLogin: {
-    marginBottom: 8,
+    marginTop: 10,
     backgroundColor: "#bbb"
   },
 
   buttonCreate: {
+    marginTop: 10,
     backgroundColor: 'green',
   },
 })
